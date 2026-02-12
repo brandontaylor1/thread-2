84c66789c830ba8bab2fc448b3b4f6578f542399
brandontaylor1 <brandon@thecreativekingdomco.com>
2026-02-12 09:07:42 -0600

    Patch vite.config.js to resolve to the new pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     base: "/thread-2/",
     build: {
         outDir: 'dist',
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                studio: resolve(__dirname, 'studio.html'),
+                approach: resolve(__dirname, 'approach.html'),
+            },
+        },
     },
     publicDir: 'public'
 })
